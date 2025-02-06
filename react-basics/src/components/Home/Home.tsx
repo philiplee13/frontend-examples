e2001d15b4d4ee0da8e7014413c0540e518f424c
Philip Lee <philiplee@MacBookPro.lan>
2025-02-06 14:40:51 -0800

    add links

--- react-basics/src/components/Home/Home.tsx
+++ react-basics/src/components/Home/Home.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router";
+
 function Home() {
   return (
     <>
       <div>
         <h1>Home Page</h1>
+        <Link to="/info">
+          <button>Info Page</button>
+        </Link>
       </div>
     </>
   );
--- react-basics/src/components/Info/Info.tsx
+++ react-basics/src/components/Info/Info.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router";
+
 function Info() {
   return (
     <>
       <h1>Info Page</h1>
+      <Link to="/">
+        <button>Home Page</button>
+      </Link>
     </>
   );
 }
