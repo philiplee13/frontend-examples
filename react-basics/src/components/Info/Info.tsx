fd3c028847bf872c19cb092e4850e96df7cbfe22
Philip Lee <philiplee@MacBookPro.lan>
2025-02-08 12:50:46 -0800

    update

--- react-basics/src/components/Info/Info.tsx
+++ react-basics/src/components/Info/Info.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router";
+import { useState, useEffect } from "react";
 
 function Info() {
   return (
@@ -8,6 +9,7 @@ function Info() {
         <button>Home Page</button>
       </Link>
       <ShowList data={data} />
+      <ShowText />
     </>
   );
 }
@@ -15,10 +17,10 @@ function Info() {
 // render lists example
 
 const data = [
-  { name: "jane", age: 5 },
-  { name: "john", age: 10 },
-  { name: "bob", age: 20 },
-  { name: "dylan", age: 30 },
+  { id: 1, name: "jane", age: 5 },
+  { id: 2, name: "john", age: 10 },
+  { id: 3, name: "bob", age: 20 },
+  { id: 4, name: "dylan", age: 30 },
 ];
 
 interface ApiData {
@@ -27,10 +29,10 @@ interface ApiData {
 
 function ShowList({ data }: ApiData) {
   const listUsers = data.map((user) => (
-    <>
+    <li key={user.id}>
       <p>user name is {user.name}</p>
       <p>user age is {user.age}</p>
-    </>
+    </li>
   ));
   return (
     <>
@@ -40,5 +42,25 @@ function ShowList({ data }: ApiData) {
 }
 
 // useEffect example
+// show inital text, wait 3 seconds, then show updated text
+// a real example would be calling an api and updating the component (like a table)
+// useEffect should be used to sync with outside sources
+function ShowText() {
+  const [text, setText] = useState<string>("nothing");
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("inside timeout"); // why does this render twice
+      // https://react.dev/learn/synchronizing-with-effects#how-to-handle-the-effect-firing-twice-in-development
+      // seems like something specific to development - but in real applications be careful
+      setText("updated text after 3 seconds");
+    }, 3000);
+  }, [text]);
+  return (
+    <>
+      <p>Text was {text}</p>
+    </>
+  );
+}
 
 export default Info;
