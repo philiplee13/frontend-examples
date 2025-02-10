da7eda8c5e554c6be3c4797089d0a3ee3bc0f305
Philip Lee <philiplee@Philips-MacBook-Pro.local>
2025-02-10 11:57:14 -0800

    mui

--- react-basics/src/components/Home/Home.tsx
+++ react-basics/src/components/Home/Home.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Paper from "@mui/material/Paper";
 
 function Home() {
   return (
@@ -14,6 +16,7 @@ function Home() {
       {/* these two buttons states should not be shared */}
       <Button name="Button 1" />
       <Button name="Button 2" />
+      <ShowTable />
     </>
   );
 }
@@ -53,6 +56,63 @@ function Button({ name }: ButtonProps) {
       <p>Click the button to increment the count {count}</p>
       <button onClick={increment}>{name}</button>
     </>
+  );
+}
+
+function ShowTable() {
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 70 },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 130,
+      sortable: false,
+    },
+    { field: "lastName", headerName: "Last name", width: 130 },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 90,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (value, row) =>
+        `${row.firstName || ""} ${row.lastName || ""}`,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
+
+  const paginationModel = { page: 0, pageSize: 5 };
+
+  return (
+    // materialUI
+    //https://mui.com/material-ui/react-table/
+    <Paper sx={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+        sx={{ border: 0 }}
+      />
+    </Paper>
   );
 }
 export default Home;
